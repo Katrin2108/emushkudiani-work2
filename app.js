b7ba6ccaf0eeaaa2d0b60bc54ae90d7bad7c4919
Katrin <126097484+Katrin2108@users.noreply.github.com>
2023-04-15 23:47:55 +0400

    end

---
+++ app.js
@@ -0,0 +1,35 @@
+//1.
+const myArrays=[2,4,6,8,10,12];
+console.log(myArrays);
+
+myArrays.pop(5);
+myArrays.unshift(12);
+console.log(myArrays);
+//2.
+const product=[
+    { productTitle:"macbook",
+      productPrice:1500,
+    },
+    { productTitle:"iphone",
+      productPrice:800,
+    },
+    { 
+      productTitle:"zbook",
+      productPrice:2500,
+    },
+];
+//3.
+const number=[2,4,6,8,10];
+console.log(0+1+2+3+4/5);//ver gavige ashkarad rogor.:)
+//4.
+const products={
+    phoneName:"samsung x",
+    phoneDescription:"All purposes",
+    phoneprice:2000,
+    shopAddresses:"mall",
+    phoneMemoryInGB:32,
+};
+//5.
+console.log(
+   ' Phone name is ${phoneName}, phone price is ${phoneprice}, and you can buy it at ${shopAddresses}'
+);//ar gamatara
